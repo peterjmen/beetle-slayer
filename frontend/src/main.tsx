52830a36a081660052119289b67fb46c885e4d63
Peter Menhinick <peter.menhinick@end-game.com>
2024-07-23 13:05:44 +1200

    adding layout and basic chakra init

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "./components/Layout/Layout.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <Layout>
+        <App />
+      </Layout>
+    </ChakraProvider>
+  </React.StrictMode>
+);
